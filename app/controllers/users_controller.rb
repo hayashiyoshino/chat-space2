6fe3821f38df0d6fdbb9e45812bd0e48018c08c2
林佳志乃 <twinklestaryoshino@gmail.com>
2018-09-15 15:35:08 +0900

    コントローラー記入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where.not(id: params[:users_id]).where('name LIKE(?)', "#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
   def edit
   end
 

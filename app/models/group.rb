adf1cd67a330936948e53ea85debfc637592e7e1
林佳志乃 <twinklestaryoshino@gmail.com>
2018-09-17 16:52:20 +0900

    ビューを修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,19 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  # after_create :create_group_user
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
+  # private
+  # def create_group_user
+  #   self.group_user = GroupUser.create(group_id: group_id, user_id: current_user.id, updated_at: updated_at)
+  # end
 end

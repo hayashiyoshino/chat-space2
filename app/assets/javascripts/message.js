24d2837445459f5c07a9d3d702cee275ece8b589
林佳志乃 <twinklestaryoshino@gmail.com>
2018-10-01 09:33:00 +0900

    definition ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,11 +2,23 @@ $(function() {
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    // FormDataはフォームの情報を取得するメソッド
+    var url = $(this).attr('action');
+    // attrメソッドは要素が持つ指定属性の値を返す
     console.log(formData);
     console.log(url);
   })
   $.ajax({
-
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    // processDataオプションはデフォルトではtrueになっており、dataに指定したオブジェクトをクエリ文字に変換する役割がある
+    contentType: false
+    // contentTypeオプションはサーバーにデータのファイル形式を伝えるヘッダ
+    // text/xmlでコンテンツタイプをXMLとして返してくる
+    // ajaxのリクエストがFormDataのときはどちらの値も適切な状態で送ることが可能なため、falseにすることで設定が上書きされることを防ぐ
+    // FormDataを使用してフォームの情報を取得した時には必ずfalseにするという認識で良い
   })
 })

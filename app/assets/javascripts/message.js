f60c379a0ba7665ff005d38bdac95d961a6cd7cd
林佳志乃 <twinklestaryoshino@gmail.com>
2018-10-01 15:38:38 +0900

    fix ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -7,8 +7,7 @@ $(function() {
     // attrメソッドは要素が持つ指定属性の値を返す
     console.log(formData);
     console.log(url);
-  })
-  $.ajax({
+    $.ajax({
     url: url,
     type: "POST",
     data: formData,
@@ -20,5 +19,6 @@ $(function() {
     // text/xmlでコンテンツタイプをXMLとして返してくる
     // ajaxのリクエストがFormDataのときはどちらの値も適切な状態で送ることが可能なため、falseにすることで設定が上書きされることを防ぐ
     // FormDataを使用してフォームの情報を取得した時には必ずfalseにするという認識で良い
+  })
   })
 })

38c95a5d14afd072c17b0f2f20e0d513288fd05e
林佳志乃 <twinklestaryoshino@gmail.com>
2018-10-03 16:37:38 +0900

    definition ajax
    edit routes.rb

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,12 +3,12 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
     console.log(input);
-    // $.ajax({
-    //   type: 'GET',
-    //   url: '/users',
-    //   data: { keyword: input },
-    //   dataType: 'json'
-    // })
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
 
   })
 

2d23f8de963b617443e8db3e174d9226d86fb230
林佳志乃 <twinklestaryoshino@gmail.com>
2018-10-18 17:20:02 +0900

    fix setting files

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.11.0"
 
 set :application, "chat-space2"
-set :repo_url, "git@example.com:hayashiyoshino/chat-space2.git"
+set :repo_url, "git@github.com:hayashiyoshino/chat-space2.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/hayashiyoshino/.ssh']
+                  keys: ['/Users/hayashiyoshino/.ssh/hayashinokey.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
